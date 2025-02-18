1413bb6c64d6a2d31c7be45c6524c6d50fa64b52
mtwatso2-eng <55025422+mtwatso2-eng@users.noreply.github.com>
2025-02-18 12:49:35 -0800

    Update assembleMetadata.R

--- modules/datasets/assembleMetadata.R
+++ modules/datasets/assembleMetadata.R
@@ -3,9 +3,19 @@ assembleMetadata <- function(metadata){
   createLink <- function(val) {
     sprintf(paste0('<a href="', URLdecode(val),'" target="_blank">', val,'</a>'))
   }
+
+  completedDatasets <- datasetDir %>%
+    list.files(full.names = TRUE, recursive = TRUE) %>%
+    {.[map_lgl(., function(x){grepl("curate_data_code.R", x)})]} %>%
+    {.[file.info(.)$size > 1]}
   
   metadata %<>%
-    select(Title, `N Geno` = N_Geno, `N Markers` = N_markers, `Has Phenotype?` = Phenotype, Tags) %>%
+    rowwise() %>%
+    mutate(
+      `In Kaggle?` = any(grepl(Folder, completedDatasets)),
+      Kaggle = ifelse(`In Kaggle?`, createLink(paste0("https://www.kaggle.com/datasets/ag2p-disc/", gsub("_", "-", tolower(Folder)))), "")
+    ) %>%
+    select(Title, `N Geno` = N_Geno, `N Markers` = N_markers, `Has Phenotype?` = Phenotype, Kaggle) %>%
     rowwise() %>%
     mutate(
       `N Geno` = as.integer(`N Geno`),
